f56263a3569e08d28c2128c016eff57d952dff67
SinYongJu <ygp007@naver.com>
2017-10-15 21:02:46 +0900

    13일 부터 진행한 프론트 엔드 공부
    풀스택을 노리면서 프론트엔드를 공부하면서 느끼는 점은 ㅋㅋㅋ 정말 깊고도 깊다 ㅋㅋㅋㅋㅋㅋㅋ like 해구!
    
    정말 수박 겉핡기가 되면 안되겠다 생각한다.

--- nodetest/nodetest3/app.js
+++ nodetest/nodetest3/app.js
@@ -21,14 +21,6 @@ var flash = require('connect-flash');
 
 
 
-// 모듈로 분리한 설정 파일 불러오기
-// var config = require('./config/config');
-
-// 모듈로 분리한 데이터베이스 파일 불러오기
-// var database = require('./database/database');
-
-// 모듈로 분리한 라우팅 파일 불러오기
-// var route_loader = require('./routes/route_loader');
 
 
 // 익스프레스 객체 생성
